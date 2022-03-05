45d96a30fcff3242ddbdd4ee8b94e1e3a077b3aa
BradleyMurrell <b.murrell89@gmail.com>
2022-03-05 00:18:11 +0000

    Defined second paths

--- run.py
+++ run.py
@@ -8,15 +8,32 @@ def start_story():
     if answer == "yes":
         path_1()
 
-    elif answer == "no":
-        print("Thank you for playing")
+    else:
+        start_story()
 
 
 def path_1():
     """
-    First path in the story
+    Tier 1
     """
-    print("Story starts here. Choose your first path. Left or right?")
+    print("Story starts here. Choose your first path. Left or right? \n")
+    answer = input("[Enter: left or right] \n")
+
+    if answer == "left":
+        path_2_a()
+
+    elif answer == "right":
+        path_2_b()
+
+    else:
+        path_1()
+
+
+def path_2_a():
+    """
+    Tier 2
+    """
+    print("You chose the left path. Choose your the next path. Left or right? \n")
     answer = input("[Enter: left or right] \n")
 
     if answer == "left":
@@ -25,4 +42,27 @@ def path_1():
     elif answer == "right":
         start_story()
 
+    else:
+        path_2_a()
+
+def path_2_b():
+    """
+    Tier 2
+    """
+    print("You chose the right path. Choose your next path. Left or right? \n")
+    answer = input("[Enter: left or right] \n")
+
+    if answer == "left":
+        start_story()
+
+    elif answer == "right":
+        start_story()
+
+    else:
+        path_2_b()
+
+
+
 path_1()
+path_2_a()
+path_2_b()
