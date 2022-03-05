4e0e71abc5e586e8c3d5538963bc7001426c6c33
BradleyMurrell <b.murrell89@gmail.com>
2022-03-05 00:34:34 +0000

    Define functions for the third path

--- run.py
+++ run.py
@@ -11,10 +11,11 @@ def start_story():
     else:
         start_story()
 
+#PATH 1
 
 def path_1():
     """
-    Tier 1
+    Path 1
     """
     print("Story starts here. Choose your first path. Left or right? \n")
     answer = input("[Enter: left or right] \n")
@@ -28,10 +29,45 @@ def path_1():
     else:
         path_1()
 
+#PATH 2
 
 def path_2_a():
     """
-    Tier 2
+    Path 2
+    """
+    print("You chose the left path. Choose your the next path. Left or right? \n")
+    answer = input("[Enter: left or right] \n")
+
+    if answer == "left":
+        path_3_a()
+
+    elif answer == "right":
+        path_3_b()
+
+    else:
+        path_2_a()
+
+def path_2_b():
+    """
+    Path 2
+    """
+    print("You chose the right path. Choose your next path. Left or right? \n")
+    answer = input("[Enter: left or right] \n")
+
+    if answer == "left":
+        path_3_c()
+
+    elif answer == "right":
+        path_3_d()
+
+    else:
+        path_2_b()
+
+#PATH 3
+
+def path_3_a():
+    """
+    Path 3
     """
     print("You chose the left path. Choose your the next path. Left or right? \n")
     answer = input("[Enter: left or right] \n")
@@ -43,11 +79,11 @@ def path_2_a():
         start_story()
 
     else:
-        path_2_a()
+        path_3_a()
 
-def path_2_b():
+def path_3_b():
     """
-    Tier 2
+    Path 3
     """
     print("You chose the right path. Choose your next path. Left or right? \n")
     answer = input("[Enter: left or right] \n")
@@ -59,10 +95,46 @@ def path_2_b():
         start_story()
 
     else:
-        path_2_b()
+        path_3_b()
+
+def path_3_c():
+    """
+    Path 3
+    """
+    print("You chose the left path. Choose your the next path. Left or right? \n")
+    answer = input("[Enter: left or right] \n")
+
+    if answer == "left":
+        start_story()
+
+    elif answer == "right":
+        start_story()
+
+    else:
+        path_3_c()
+
+def path_3_d():
+    """
+    Path 3
+    """
+    print("You chose the right path. Choose your next path. Left or right? \n")
+    answer = input("[Enter: left or right] \n")
+
+    if answer == "left":
+        start_story()
+
+    elif answer == "right":
+        start_story()
+
+    else:
+        path_3_d()
 
 
 
 path_1()
 path_2_a()
 path_2_b()
+path_3_a()
+path_3_b()
+path_3_c()
+path_3_d()
