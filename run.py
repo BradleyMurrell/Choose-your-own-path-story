b2644db48077d04ff3577067313b8d7ef2fc482f
BradleyMurrell <b.murrell89@gmail.com>
2022-03-11 22:22:56 +0000

    Define dotted line

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ Imports go here
 """
 import random
 
+
 print("-" * 50)
-print(" A messengers journey")
+print("A messengers journey")
 print("-" * 50)
-print(" Instructions")
+print("Instructions")
 print("-" * 50)
-character_name = input(" Please enter your character's name: \n")
+character_name = input("Please enter your character's name: \n")
 print("-" * 50)
 
 
@@ -16,11 +17,11 @@ def start_story():
     """
     Start a new story here
     """
-    print("-" * 50)
-    print("\n Do you want to start the story again? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: yes or no] \n")
-    print("-" * 50)
+    dotted_line()
+    print("Do you want to start the story again?")
+    dotted_line()
+    answer = input("[Enter: yes or no] ")
+    dotted_line()
 
     if answer == "yes":
         path_1()
@@ -33,17 +34,17 @@ def path_1():
     """
     Path 1
     """
-    print("-" * 50)
-    print(f"\n A messenger named {character_name} has been tasked to deliver \
+    dotted_line()
+    print(f"A messenger named {character_name} has been tasked to deliver \
 an important message to the castle. Along the way, {character_name} came to \
 a crossroad. To the left, the path led through mountains. To the right, the \
 path led through a dark and scary forest. The mountains look safer, but will \
 take a few extra days. The forest would be quicker, but it looks dangerous. \
-Does {character_name} take the left path or the right? \n")
-    print("-" * 50)
+Does {character_name} take the left path or the right?")
+    dotted_line()
 
-    answer = input("\n [Enter: left or right] \n")
-    print("-" * 50)
+    answer = input("[Enter: left or right] ")
+    dotted_line()
 
     if answer == "left":
         path_2_a()
@@ -52,23 +53,23 @@ Does {character_name} take the left path or the right? \n")
         path_2_b()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: left or right] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: left or right] ")
 
 
 def path_2_a():
     """
     Path 2
     """
-    print("-" * 50)
-    print(f"\n {character_name} decided that it would be safer to take the \
+    dotted_line()
+    print(f"{character_name} decided that it would be safer to take the \
 left path through the mountains. Before the ascent, {character_name} passed \
 a merchant who was selling supplies. Does {character_name} stop to buy \
-supplies before moving on? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: buy supplies or keep moving] \n")
-    print("-" * 50)
+supplies before moving on?")
+    dotted_line()
+    answer = input("[Enter: buy supplies or keep moving] ")
+    dotted_line()
 
     if answer == "buy supplies":
         path_3_a()
@@ -77,21 +78,21 @@ supplies before moving on? \n")
         path_3_b()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: buy supplies or keep moving] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: buy supplies or keep moving] ")
 
 
 def path_2_b():
     """
     Path 2
     """
-    print("-" * 50)
-    print("\n You chose the right path. Choose your next path. Left \
-    or right? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: left or right] \n")
-    print("-" * 50)
+    dotted_line()
+    print("You chose the right path. Choose your next path. Left \
+or right?")
+    dotted_line()
+    answer = input("[Enter: left or right] ")
+    dotted_line()
 
     if answer == "left":
         path_3_c()
@@ -100,34 +101,34 @@ def path_2_b():
         path_3_d()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: left or right] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: left or right] ")
 
 
 def path_3_a():
     """
     Path 3
     """
-    print("-" * 50)
-    print(f"\n As {character_name} was busy looking at the supplies, \
+    dotted_line()
+    print(f"As {character_name} was busy looking at the supplies, \
 the merchant, who was actually an assasin, snuck up behind {character_name} \
-and attempted to attack.\n")
-    print("-" * 50)
-    print(" Attempt to fight back? [Roll dice. Roll a 3, 4, 5 or 6 to win]")
-    answer = input("\n [Enter: fight or don't fight] \n")
-    print("-" * 50)
+and attempted to attack.")
+    dotted_line()
+    print("Attempt to fight back? [Roll dice. Roll a 3, 4, 5 or 6 to win]")
+    answer = input("[Enter: fight or don't fight] ")
+    dotted_line()
     if answer == "fight":
         dice_roll = random.randrange(1, 6)
-        print(f" You rolled a {dice_roll}")
+        print(f"You rolled a {dice_roll}")
 
         if dice_roll >= 3:
-            print(f" {character_name} successfully countered the assasin and \
-\n defeated him.")
+            print(f"{character_name} successfully countered the assasin and \
+defeated him.")
             path_3_b()
 
         else:
-            print(f" {character_name} failed to counter the assasins attacked \
+            print(f"{character_name} failed to counter the assasins attacked \
 and died.")
             start_story()
 
@@ -136,23 +137,23 @@ and died.")
         start_story()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: fight or don't fight] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: fight or don't fight] ")
 
 
 def path_3_b():
     """
     Path 3
     """
-    print("-" * 50)
-    print(f"\n {character_name} continues the journey through the mountains. \
+    dotted_line()
+    print(f"{character_name} continues the journey through the mountains. \
 Three days have past and {character_name} can finally see the castle in the \
 distance. Does {character_name} continue to the castle or make camp and rest \
-for the night? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: continue or rest] \n")
-    print("-" * 50)
+for the night?")
+    dotted_line()
+    answer = input("[Enter: continue or rest] ")
+    dotted_line()
 
     if answer == "continue":
         finish_story()
@@ -161,21 +162,21 @@ for the night? \n")
         path_4_a()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: continue or rest] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: continue or rest] ")
 
 
 def path_3_c():
     """
     Path 3
     """
-    print("-" * 50)
-    print("\n You chose the left path. Choose your the next path. Left \
-    or right? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: left or right] \n")
-    print("-" * 50)
+    dotted_line()
+    print("You chose the left path. Choose your the next path. Left \
+or right?")
+    dotted_line()
+    answer = input("[Enter: left or right] ")
+    dotted_line()
 
     if answer == "left":
         start_story()
@@ -184,21 +185,21 @@ def path_3_c():
         start_story()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: left or right] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: left or right] ")
 
 
 def path_3_d():
     """
     Path 3
     """
-    print("-" * 50)
-    print("\n You chose the right path. Choose your next path. Left \
-    or right? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: left or right] \n")
-    print("-" * 50)
+    dotted_line()
+    print("You chose the right path. Choose your next path. Left \
+or right?")
+    dotted_line()
+    answer = input("[Enter: left or right] ")
+    dotted_line()
 
     if answer == "left":
         start_story()
@@ -207,21 +208,21 @@ def path_3_d():
         start_story()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: left or right] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: left or right] ")
 
 
 def path_4_a():
     """
     Path 4
     """
-    print("-" * 50)
-    print("\n You chose the right path. Choose your next path. Left \
-    or right? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: left or right] \n")
-    print("-" * 50)
+    dotted_line()
+    print("You chose the right path. Choose your next path. Left \
+or right?")
+    dotted_line()
+    answer = input("[Enter: left or right] ")
+    dotted_line()
 
     if answer == "left":
         start_story()
@@ -230,21 +231,21 @@ def path_4_a():
         start_story()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: left or right] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: left or right] ")
 
 
 def path_4_b():
     """
     Path 4
     """
-    print("-" * 50)
-    print("\n You chose the right path. Choose your next path. Left \
-    or right? \n")
-    print("-" * 50)
-    answer = input("\n [Enter: left or right] \n")
-    print("-" * 50)
+    dotted_line()
+    print("You chose the right path. Choose your next path. Left \
+or right?")
+    dotted_line()
+    answer = input("[Enter: left or right] ")
+    dotted_line()
 
     if answer == "left":
         start_story()
@@ -253,9 +254,9 @@ def path_4_b():
         start_story()
 
     else:
-        print(" Invalid input")
-        print("-" * 50)
-        answer = input("\n [Enter: left or right] \n")
+        print("Invalid input")
+        dotted_line()
+        answer = input("[Enter: left or right] ")
 
 
 def finish_story():
@@ -266,12 +267,13 @@ def finish_story():
     start_story()
 
 
-def game_over():
+def dotted_line():
     """
-    When the story ends before making it to the end
+    Prints a dotted line with a new line above and below
     """
-    print("\n Game over \n")
-    start_story()
+    print("\n")
+    print("-" * 60)
+    print("\n")
 
 
 path_1()
@@ -285,3 +287,4 @@ path_4_a()
 path_4_b()
 start_story()
 finish_story()
+dotted_line()
