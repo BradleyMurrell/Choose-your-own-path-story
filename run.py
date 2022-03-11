6c13418d11bbc9ee3c80b070852f8e1e9fccd0d0
BradleyMurrell <b.murrell89@gmail.com>
2022-03-11 21:24:05 +0000

    Colorama test

--- run.py
+++ run.py
@@ -2,12 +2,15 @@
 Imports go here
 """
 import random
-from colorama import Fore
+from colorama import Fore, Back, Style
 
-
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
 print("-" * 50)
 print(" A messengers journey")
-print(Fore.RED + 'some red text')
 print("-" * 50)
 print(" Instructions")
 print("-" * 50)
